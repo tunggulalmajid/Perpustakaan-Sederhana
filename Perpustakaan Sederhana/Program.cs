0d7829f77d179e0cd2c6bd67348f0855e23a6104
tunggulalmajid <tunggulmajid@gmail.com>
2025-03-02 16:47:16 +0700

    Add project files.

---
+++ Perpustakaan Sederhana/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Timers;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        header();
+
+        Console.WriteLine("Menu : ");
+        Console.WriteLine("1. Tambahkan Buku");
+        Console.WriteLine("2. List Buku");
+        Console.WriteLine("3. Hapus Buku");
+        Console.WriteLine("4. Keluar");
+        garis(60);
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("Masukkan Opsi Yang Ingin Dipilih >> ");
+                int pilihOpsi = Convert.ToInt32(Console.ReadLine());
+                if (pilihOpsi == 1)
+                {
+                    Console.WriteLine("haha");
+                }
+                else if (pilihOpsi == 2)
+                {
+                    Console.WriteLine("haha");
+
+                }
+                else if (pilihOpsi == 3)
+                {
+                    Console.WriteLine("haha");
+
+                }
+                else if (pilihOpsi == 4)
+                {
+                    Console.WriteLine("haha");
+
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        Console.ReadLine();
+    }
+    static void garis(int n)
+    {
+        Console.WriteLine(new string('═', n));
+    }
+    static void header()
+    {
+        garis(60);
+        Console.WriteLine("PERPUSTAKAAN SEDERHANA".PadLeft(40));
+        garis(60);
+
+    }
+}
